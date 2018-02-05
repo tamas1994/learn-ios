60beb7a3a241cc86998dbfc62d431b42d56470eb
tamas1994 <donghaifeng@hotmail.com>
2018-02-05 17:44:16 +0800

    storyboard与swift类绑定

---
+++ learn-ios/MyViewController.swift
@@ -0,0 +1,48 @@
+//
+//  MyViewController.swift
+//  learn-ios
+//
+//  Created by Tamas Dong on 2018/2/5.
+//  Copyright © 2018年 Tamas Dong. All rights reserved.
+//
+
+import UIKit
+
+class MyViewController: UIViewController {
+    
+    /*
+    init(coder aDecoder:NSCoder!){
+        super.init(coder:aDecoder)
+    }
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    
+    }
+    */
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("viewDidLoad called ")
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
